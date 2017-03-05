e0890503194971ae089edeebc5fb29b4e7e12652
girltech <mariacutipa.gonzales@gmail.com>
2017-03-04 20:37:54 -0500

    Correct Update
    Demostracion de como alinear un div en Js con el 
    result.setAttribute('style','font-size: 20px; cursor: pointer; text-align:center;');

--- js/main.js
+++ js/main.js
@@ -2,25 +2,26 @@ function translate(){
 	var tittle = document.getElementById("form-signin-heading");
 	var email = document.getElementById("inputEmail");
 	var password = document.getElementById("inputPassword");
-	var button = document.getElementById("btn");
-	var remember =document.getElementsByTagName("span")[0];
-	//var button =document.getElementsByTagName("btn")[0];
+	var remember =document.getElementsByTagName('span')[0];
+	var button = document.getElementsByClassName('btn')[0];
 
 	tittle.innerHTML = "Por favor inicia sesión";
 	email.placeholder = "Correo electrónico";
 	password.placeholder = "Contraseña";
-	remember.innerHTML = "Recordar datos";
 	button.innerHTML = "Iniciar sesión";
+	remember.innerHTML = "Recordar datos";
+	
 }
 translate();
 
 
 function mostrar(){
-	var email = document.getElementById("inputEmail").value;
-	var password = document.getElementById("inputPassword").value;
-	var resulta =document.getElementById("result");
-
-     var clase = document.getElementsByClassName("ver").value;
-
-	resulta.innerHTML = clase[0].value;
-}
+  var clase = document.getElementsByClassName('form-control');
+  var result =document.getElementById('result');
+  result.setAttribute('style','font-size: 20px; cursor: pointer; text-align:center;'); //centrar el div
+  result.innerHTML = "<h3>"+"Datos de formulario"+"</h3>"+"<br>"+
+    					"El correo electrónico ingresado es:"+"<br>"+
+    					clase[0].value+"<br>"+
+    					"La contraseña ingresada es:"+"<br>"+
+    					clase[1].value;
+ }
